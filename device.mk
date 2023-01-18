a3ff89a5e90db2cef3b1891c7b88d88603d686fd
rabilgic <rabilgic@outlook.com>
2023-01-18 16:24:36 +0300

    Initial commit

---
+++ device.mk
@@ -0,0 +1,74 @@
+# Inherit from common AOSP config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from virtual AB OTA config
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+LOCAL_PATH := device/oneplus/hotdogb
+
+# define hardware platform
+PRODUCT_PLATFORM := msmnile
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+    
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+# Userdata Checkpointing OTA GC
+PRODUCT_PACKAGES += \
+    checkpoint_gc
+    
+# tell update_engine to not change dynamic partition table during updates
+# needed since our qti_dynamic_partitions does not include
+# vendor and odm and we also dont want to AB update them
+#TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-impl-wrapper.recovery \
+    android.hardware.boot@1.0-impl-wrapper \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery \
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd \
+    resetprop
+
+# qcom decryption
+PRODUCT_PACKAGES_ENG += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# tzdata
+PRODUCT_PACKAGES_ENG += \
+    tzdata_twrp
+
+# Apex libraries
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
---
+++ recovery/root/system/bin/unified-script.sh
@@ -0,0 +1,48 @@
+#!/system/bin/sh
+# This script is needed to automatically set device props.
+
+
+load_op7tpro()
+{
+    resetprop "ro.product.model" "OnePlus 7T Pro"
+    resetprop "ro.product.name" "OnePlus7TPro"
+    resetprop "ro.build.product" "OnePlus7TPro"
+    resetprop "ro.product.device" "OnePlus7TPro"
+    resetprop "ro.vendor.product.device" "OnePlus7TPro"
+    resetprop "ro.display.series" "OnePlus 7T Pro"
+}
+
+load_op7tpro5g()
+{
+    resetprop "ro.product.model" "OnePlus 7T Pro 5G"
+    resetprop "ro.product.name" "OnePlus7TProNR"
+    resetprop "ro.build.product" "OnePlus7TProNR"
+    resetprop "ro.product.device" "OnePlus7TProNR"
+    resetprop "ro.vendor.product.device" "OnePlus7TProNR"
+    resetprop "ro.display.series" "OnePlus 7T Pro 5G"
+}
+
+load_op7t()
+{
+    resetprop "ro.product.model" "OnePlus 7T"
+    resetprop "ro.product.name" "OnePlus7T"
+    resetprop "ro.build.product" "OnePlus7T"
+    resetprop "ro.product.device" "OnePlus7T"
+    resetprop "ro.vendor.product.device" "OnePlus7T"
+    resetprop "ro.display.series" "OnePlus 7T"
+}
+
+project=$(getprop ro.boot.project_name)
+echo $project
+
+case $project in
+    "19801")
+        load_op7tpro
+        ;;
+    "18865")
+        load_op7t
+        ;;
+    *)
+esac
+
+exit 0
